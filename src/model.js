fca7a900097097efe8a05c3643b9ca4f0133d242
alptibet <a.tibet@yandex.ru>
2021-06-08 14:23:20 +0300

    solver generating uuid

--- src/model.js
+++ src/model.js
@@ -1,22 +1,32 @@
 export const state = [];
 
-const toDoItem = function (data) {
+const createToDoItem = function (data) {
   return {
-    todo: 'deneme',
-    id: 1,
+    todo: data,
+    id: createUUID(),
     completed: false,
   };
 };
 
 export const addToDo = function (todo) {
-  state.push(toDoItem(todo));
+  state.push(createToDoItem(todo));
   console.log(state);
 };
 
-const deleteToDo = function (todoId) {
+const deleteToDo = function (todo) {
   //code here
 };
 
-const completeTodo = function (todoId) {
+const completeTodo = function (todo) {
   //code here
+};
+
+const createUUID = function () {
+  let dt = new Date().getTime();
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
 };
